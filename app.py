6acf6bd4e71c290ce2d2f03094f77343e4418eb3
Anoopa Singh <63153728+anoopasingh@users.noreply.github.com>
2020-06-22 13:30:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import fortunegetter
+from datetime import datetime 
+
 
 
 # -- Initialization section --
@@ -13,4 +16,14 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html', time=datetime.now())
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'POST':
+        name = request.form["formname"]
+        fortune = request.form["fortune"]
+        fortuneteller = fortunegetter()
+        return render_template("results.html",  name=name, fortuneteller = fortuneteller)
+    else:
+        return render_template("index.html")
